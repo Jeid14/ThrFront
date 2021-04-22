418dc546d71e48b87bc5ac3f5384311ecf657d49
Евгений <jeid14@gmail.com>
2021-04-22 15:54:46 +0300

    first

---
+++ src/main/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const pages = [{
+    template: './login/index.html',
+    filename: 'index.html',
+    chunks: ['main']
+},
+    // {
+    //     template: './chat/chat.html',
+    //     filename: 'chat.html',
+    //     chunks: ['chat']
+    // },
+    // {
+    //     template: './signup/signUp.html',
+    //     filename: 'signUp.html',
+    //     chunks: ['registration']
+    // }
+]
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './login/main.js',
+        // chat: './chat/script.js',
+        // registration: './signup/script.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        ...pages.map((pageConfig) => new HTMLWebpackPlugin(pageConfig)),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        port: 4200,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(jpg|svg|gif|png)/,
+                use: ['file-loader'],
+            }
+        ]
+    }
+}
